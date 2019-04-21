13d1e6678dd83fcbd40d262fc54ae26fbed8e46e
CcCc <632582282@qq.com>
2019-04-21 11:19:00 +0800

    by cjx

--- 2-oop/01.py
+++ 2-oop/01.py
@@ -11,7 +11,7 @@ class Student():
 anran = Student()
 
 # 定义一个描述听python的学生的类
-class PythonStudent():
+class Student():
     name = None  # 用None给不确定的值赋值
     age = 18
     course = "python"
@@ -20,7 +20,50 @@ class PythonStudent():
         return None   # 函数末尾加return
 
 # 实例化一个叫anran的学生，是一个具体的人
-anran = PythonStudent()
+anran = Student()
 print(anran.age)
 print(anran.name)
 anran.DoHomeWork()  # 成员函数的调用没有传递进入参数
+##############################
+print(Student.__dict__)
+print(anran.__dict__)
+##############################
+print(id(Student.age))
+print(id(Student.name))
+print("*" * 50)
+print(id(anran.age))
+print(id(anran.name))  # 此处id相同，说明指向的是同一个变量,但如果给anran中的属性赋和类中属性不同的值，则id会发生变化
+anran.age = 18
+anran.name = "anran"
+print("*" * 50)
+print(id(anran.age))
+print(id(anran.name))
+print(anran.__dict__)
+
+class Student():
+    def say(self):
+        self.name = "cjx"   # 将self替换成cjx，此处变成 cjx.name
+        self.age = 23
+        print("my name is {0}".format(self.name))
+        print("my age is {0}".format(self.age))
+    def sayagain(s):
+        print("my name is {0}".format(s.name))  # s也用cjx代替
+        print("my age is {0}".format(s.age))
+cjx = Student()
+cjx.say()
+cjx.sayagain()
+print("*" * 50)
+
+class Teacher():
+    name = "dana"
+    age = 40
+    def say(self):
+        self.name = "yaona"
+        self.age = 23
+        print("my name is {0}".format(self.name))
+        print("my age is {0}".format(self.age))
+    def sayagain():  # 此处为绑定类方法
+        print("hello,nice to see you")
+t = Teacher()
+t.say()
+Teacher.sayagain()  # 调用绑定类方法必须使用类名
